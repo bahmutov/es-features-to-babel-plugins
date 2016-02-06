6279240a4be33ea9a657122040b3a2172f9affcc
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-02-06 10:19:22 -0500

    feat(default): added mapping for default parameter

--- index.js
+++ index.js
@@ -3,14 +3,15 @@
 var INCLUDE_BABEL_POLYFILL = 'INCLUDE_BABEL_POLYFILL'
 
 var babelMapping = {
-  letConst: 'transform-es2015-block-scoping',
-  templateString: 'transform-es2015-template-literals',
   arrow: 'transform-es2015-arrow-functions',
-  parameterDestructuring: ['transform-es2015-parameters', 'transform-es2015-destructuring'],
-  numericLiteral: 'transform-es2015-literals',
-  spreadRest: 'transform-es2015-spread',
   conciseMethodProperty: 'transform-es2015-shorthand-properties',
+  defaultParameter: 'transform-es2015-parameters',
   destructuring: 'transform-es2015-destructuring',
+  letConst: 'transform-es2015-block-scoping',
+  numericLiteral: 'transform-es2015-literals',
+  parameterDestructuring: ['transform-es2015-parameters', 'transform-es2015-destructuring'],
+  spreadRest: 'transform-es2015-spread',
+  templateString: 'transform-es2015-template-literals',
   INCLUDE_BABEL_POLYFILL: INCLUDE_BABEL_POLYFILL
 };
 
